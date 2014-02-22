3f7fd7e445f72eb316e92d61290f2df8753be4e0
Martin Atkins <mart@degeneration.co.uk>
2014-02-22 11:44:19 -0800

    Python module boilerplate.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="camlistore",
+    version="dev",
+    description="Camlistore Client Library",
+    packages=['camlistore'],
+    author="Martin Atkins",
+    author_email="mart@degeneration.co.uk",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+    ]
+)
