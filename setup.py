2ca0acc4927e21ce0e834eb716d2edf1bcbca15a
Martin Atkins <mart@degeneration.co.uk>
2014-02-22 13:34:07 -0800

    Basic setup for unit testing.

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
 
 setup(
     name="camlistore",
     version="dev",
     description="Camlistore Client Library",
-    packages=['camlistore'],
+    packages=find_packages(),
     author="Martin Atkins",
     author_email="mart@degeneration.co.uk",
+
+    test_suite='nose.collector',
+
+    setup_requires=[
+        'nose>=1.0',
+    ],
+    tests_require=[
+        'nose>=1.0',
+        'coverage',
+        'mock',
+        'pep8',
+    ],
+    install_requires=[
+        "requests",
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
